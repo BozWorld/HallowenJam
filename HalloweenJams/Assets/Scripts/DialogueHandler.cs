879e6769ad359eb1faf6e92890a6514c33fe8a46
MoevaMondoha <mondohamoeva@hotmail.fr>
2020-09-27 17:29:24 +0200

    [update dialogue system]

--- HalloweenJams/Assets/Scripts/DialogueHandler.cs
+++ HalloweenJams/Assets/Scripts/DialogueHandler.cs
@@ -11,7 +11,7 @@ public class DialogueHandler : MonoBehaviour
     public string Name;
     public GameObject continueButton;
     [SerializeField] private List<GameObject> _ButtonInteraction = null;
-    //public Player _Player;  
+    public Player _Player;  
     public float typingSpeed;
     [SerializeField] private List<DialogueLine> _dialogueList = new List<DialogueLine>();
     private int index;
@@ -20,7 +20,6 @@ public class DialogueHandler : MonoBehaviour
     
     private void Start() 
     {
-        
     }
     void Update()
     {
@@ -42,55 +41,55 @@ public class DialogueHandler : MonoBehaviour
     //gère les différent type de text et la continuation du text
     public void ContinueText()
     {
-        //continueButton.SetActive(false);
-            Debug.Log("ok");
+        if (this.gameObject.activeSelf)
+        {
+            continueButton.SetActive(false);
+        if (index < _dialogueList.Count -1 )
+        {
+            Debug.Log(index);
             index = _dialogueList[index].nextLineIndex;
             textDisplay.text = "";
             StartCoroutine(Type());
-         
+        }
         switch (_dialogueList[index].type)
         {
             case DialogueLine.DialogueType.GoodEnd:
                 textDisplay.text = "";
-                //_Player.canPlayerInteract = true;
-                //_Player.minionsCount +=1;
+                _Player.canPlayerInteract = true;
+                _Player.minionsCount +=1;
                 GameObject.Find("UiManager").SetActive(false);
                 break;
             case DialogueLine.DialogueType.BadEnd:
                 textDisplay.text = "";
-                //_Player.canPlayerInteract = true;
+                _Player.canPlayerInteract = true;
                 GameObject.Find("UiManager").SetActive(false);
                 break;
             case DialogueLine.DialogueType.Choice:
-                int y = 0;
                 for (int i = 0; i < _ButtonInteraction.Count; i++)
                 {
                 _ButtonInteraction[i] = GameObject.Find("UiManager").GetComponent<UiManager>().ChoiceButton[i];   
-                int __choiceIdx = _dialogueList[index].ChoiceIdx[y];
-                _ButtonInteraction[y].gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _dialogueList[__choiceIdx].text;
+                int __choiceIdx = _dialogueList[index].ChoiceIdx[i];
+                _ButtonInteraction[i].gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _dialogueList[__choiceIdx].text;
                 _ButtonInteraction[i].SetActive(true);
-                y++; 
                 }
-                foreach (GameObject i in _ButtonInteraction)
-                {
-                
-                }
-                 //int choiceIdx = _dialogueList[index].ChoiceIdx1;
-                //_ButtonInteraction[0].gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _dialogueList[choiceIdx].text;
                 break;
-    
             default:
                 break;
+        }  
         }
+        
     }
-    public void ChoiceMaker(int ChoiceParameter)
+    public void ChoiceMaker(int ChoiceParameter = 1)
     {
+        if (this.gameObject.activeSelf){
             index = ChoiceParameter; 
             textDisplay.text = "";
             StartCoroutine(Type());
+        }
+            
     }
-    private void OnEnable() {
-        //_Player.canPlayerInteract = false;
+    public void OnEnable() {
+        _Player.canPlayerInteract = false;
         GameObject.Find("UiManager").SetActive(true);
         StartCoroutine(Type());
         nameZone.GetComponent<TextMeshProUGUI>().text = name;
