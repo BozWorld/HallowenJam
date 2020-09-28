3b63c5b44af81af875c0e1c4bb08d3c91ec1e84f
MoevaMondoha <mondohamoeva@hotmail.fr>
2020-09-28 13:42:39 +0200

    [characterIntegration]

---
+++ HalloweenJams/Assets/Scripts/Sound.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+[System.Serializable]
+public class Sound : MonoBehaviour
+{
+    public string audioName; 
+    public AudioClip clip;
+    [Range(0f,1f)]
+    public float volume;
+    [Range(0f,1f)]
+    public float pitch;
+    [HideInInspector]
+    public AudioSource source;
+    public bool loop;
+}
